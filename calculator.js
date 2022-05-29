ccdb56b5d5695a60da7e2ab66bcf701bbb088641
Matheus-IT <matheuscosta.tech@gmail.com>
2022-05-29 08:12:08 -0300

    Calculation of sum

--- calculator.js
+++ calculator.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', function(req, res) {
+	const { num1:n1, num2:n2 } = req.body;
+	const result = Number(n1) + Number(n2);
+	res.send(`<h1>The result is ${result}</h1>`);
 });
 
 const port = 3000;
